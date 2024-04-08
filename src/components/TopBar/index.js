197d340bc3d7b9805d19bef6640e8a00b95ad0d9
karthi20021998 <karthikeyan2434@gmail.com>
2024-04-08 11:36:11 +0530

    commit

---
+++ src/components/TopBar/index.js
@@ -0,0 +1,32 @@
+import "./index.css";
+
+const TopBar = () => (
+  <div className="topbar-bg">
+    <div className="content-card-1">
+      <img
+        src="https://res.cloudinary.com/dghnaymwn/image/upload/v1712462124/appscrip/element-4_kgokcm.png"
+        alt="topbar-logo"
+        className="topbar-image"
+      />
+      <h1 className="topbar-text">Lorem ipsum dolor</h1>
+    </div>
+    <div className="content-card-2">
+      <img
+        src="https://res.cloudinary.com/dghnaymwn/image/upload/v1712462124/appscrip/element-4_kgokcm.png"
+        alt="topbar-logo"
+        className="topbar-image"
+      />
+      <h1 className="topbar-text">Lorem ipsum dolor</h1>
+    </div>
+    <div className="content-card-3">
+      <img
+        src="https://res.cloudinary.com/dghnaymwn/image/upload/v1712462124/appscrip/element-4_kgokcm.png"
+        alt="topbar-logo"
+        className="topbar-image"
+      />
+      <h1 className="topbar-text">Lorem ipsum dolor</h1>
+    </div>
+  </div>
+);
+
+export default TopBar;
